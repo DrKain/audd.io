6b9ccc2f9dde90f4e3cc0cfd97c4d2da0bc74ac0
Kain <drkain@outlook.com>
2021-03-25 10:19:39 +1300

    1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# audd.io
-A NodeJS package used to interact with the music recognition API provided by Audd.io
+# NPM audd.io
+
+[![NPM](https://img.shields.io/npm/v/audd.io)](https://www.npmjs.com/package/audd.io) [![NPM](https://img.shields.io/npm/dt/audd.io)](https://www.npmjs.com/package/audd.io) [![NPM](https://img.shields.io/npm/types/audd.io)](https://www.npmjs.com/package/audd.io)
+  
+ **What is this?**
+------------------
+A NodeJS package used to interact with the music recognition API provided by [Audd.io](https://docs.audd.io/) in the easiest way possible.  
+You can get an `api_token` for free at [https://dashboard.audd.io/](https://dashboard.audd.io/).  
+
+#### Install  
+```npm install audd.io```
+
+#### Usage:   
+```javascript
+import { Audd } from 'audd.io';
+const audd = new Audd('api_key');  
+
+audd.fromURL('web.com/file.mp3').then(console.log);
+```  
+  
+#### fromURL(url)  
+The default API method. It's for the usual music recognition that works like Shazam.  
+Attempt to match a song using an externally linked video or audio file.  
+  
+```javascript  
+audd.fromURL('https://example.com/test.mp3').then((response) => {
+    // response.result is null if no match is found
+    if (response.result) {
+        console.log(`The song is ${response.result.title} by ${response.result.artist}`)
+    } else {
+        console.log('Unable to find a match');
+    }
+}, console.log);
+```  
+  
+#### fromFile(file)  
+The default API method. It's for the usual music recognition that works like Shazam.  
+Attempt to match a song using a local video or audio file.  
+```javascript  
+audd.fromFile('path/to/file.mp3').then((response) => {
+    if (response.result) {
+        console.log(`The song is ${response.result.title} by ${response.result.artist}`)
+    } else {
+        console.log('Unable to find a match');
+    }
+}, console.log);
+```  
+  
+#### guessFromURL(url)  
+The method for recognition by **humming/singing**. We can't guarantee any accuracy for this method.  
+Attempt to guess a song using an externally linked url.  
+  
+```javascript
+audd.guessFromURL('https://example.com/test.mp3').then((response) => {
+    const result = response.result;
+    if (result && result.count > 0) {
+        result.list.map(item => console.log(`${item.title} by ${item.artist} (score: ${item.score})`))
+    } else console.log('Unable to match any songs');
+}, console.log);
+```
+  
+#### guessFromFile(url)  
+The method for recognition by **humming/singing**. We can't guarantee any accuracy for this method.  
+Attempt to match a song using a local video or audio file.   
+  
+```javascript
+audd.guessFromFile('path/to/file.mp3').then((response) => {
+    const result = response.result;
+    if (result && result.count > 0) {
+        result.list.map(item => console.log(`${item.title} by ${item.artist} (score: ${item.score})`))
+    } else console.log('Unable to match any songs');
+}, console.log);
+```
---
+++ lib/interface.d.ts
@@ -0,0 +1,149 @@
+/// <reference types="node" />
+import { URLSearchParams } from 'url';
+export interface IResponse {
+    /** error or success. Determines if the request was successful */
+    status: 'error' | 'success' | string;
+    /** Returned when a successful match was made. null if unable to match */
+    result?: IResult | null;
+    /** Contains information about the error */
+    error?: IError;
+    /** Contains a list of parameters you passed. Only displayed when the request fails */
+    requested_params?: IParams;
+}
+export interface IGuess {
+    status: 'error' | 'success' | string;
+    error?: IError;
+    result: IGuessResult | null;
+}
+interface IGuessResult {
+    underground: string;
+    humming: boolean;
+    count: number;
+    list: IGuessItem[];
+}
+interface IGuessItem {
+    score: number;
+    artist: string;
+    title: string;
+}
+export interface IData {
+    method: 'GET' | 'POST';
+    params?: URLSearchParams;
+    body?: {
+        api_token: string;
+        url: string;
+    };
+    headers?: {
+        [key: string]: any;
+    };
+    path: string;
+}
+interface IError {
+    /** Error Code */
+    error_code: number;
+    /** Message detailing why the request failed */
+    error_message: string;
+}
+interface IParams {
+    url: string;
+    method: string;
+}
+interface IResult {
+    /** Artist name */
+    artist: string;
+    /** Song title */
+    title: string;
+    /** Song Album */
+    album: string;
+    release_date: string;
+    label: string;
+    timecode: string;
+    /** Provides a lis.tn link to find the track across multiple services */
+    song_link: string;
+    apple_music?: AppleMusic;
+    spotify?: Spotify;
+}
+interface AppleArtwork {
+    width: number;
+    height: number;
+    url: string;
+    bgColor: string;
+    textColor1: string;
+    textColor2: string;
+    textColor3: string;
+    textColor4: string;
+}
+interface ApplePreview {
+    url: string;
+}
+interface AppleMusic {
+    previews: ApplePreview[];
+    artwork: AppleArtwork;
+    artistName: string;
+    url: string;
+    discNumber: number;
+    genreNames: string[];
+    durationInMillis: number;
+    releaseDate: string;
+    name: string;
+    isrc: string;
+    albumName: string;
+    playParams: {
+        id: string;
+        kind: string;
+    };
+    trackNumber: number;
+    composerName: string;
+}
+interface Spotify {
+    album: SpotifyAlbum;
+    artists: Artist[];
+    available_markets?: any;
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: {
+        isrc: string;
+    };
+    external_urls: SpotifyLinks;
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    track_number: number;
+    type: string;
+    uri: string;
+}
+interface Image {
+    height: number;
+    url: string;
+    width: number;
+}
+interface SpotifyAlbum {
+    album_type: string;
+    artists: Artist[];
+    available_markets?: any;
+    external_urls: SpotifyLinks;
+    href: string;
+    id: string;
+    images: Image[];
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    total_tracks: number;
+    type: string;
+    uri: string;
+}
+interface SpotifyLinks {
+    spotify: string;
+}
+interface Artist {
+    external_urls: SpotifyLinks;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+}
+export {};
